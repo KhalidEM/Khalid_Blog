c9744366e6a5bcd04ab0df3d017082ae4cfda5ec
El Moussaoui Khalid (s108709) <khalid.elmoussaoui@student.ap.be>
2022-02-13 23:30:13 +0100

    First Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -67,7 +67,7 @@ const Navbar = class extends React.Component {
                 About
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Product
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
